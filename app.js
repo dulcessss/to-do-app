e80be4acd7947992927b860be5e07022b7f6c4fc
dulcessss <dulce.aguilera25@gmail.com>
2017-11-15 12:45:55 -0600

    Changes for review

--- app.js
+++ app.js
@@ -1,48 +1,52 @@
-function onReady(){
-//an array of to-dos
-  const toDos = [];
-  const addToDoForm = document.getElementById('addToDoForm');
+function onReady() {
+  let newNum = 0;
+    let toDos = [];
+    const addToDoForm = document.getElementById('addToDoForm');
     function createNewToDo(){
-//accesses the text input
-      const newToDoText = document.getElementById('newToDoText');
-/*prevents a user from submitting an empty to do item.
-Executes a return if newToDoText is false */
-        if(!newToDoText.value) {return;}
-
-      toDos.push({
-        title: newToDoText.value,
-        complete: false
-      });
-      newToDoText.value = '';
-      renderTheUI();
+        const newToDoText = document.getElementById('newToDoText');
+        if(!newToDoText.value){return;}
+        toDos.push ({
+            title: newToDoText.value,
+            complete: false,
+      id: newNum
+        });
+        newToDoText.value='';
+    newNum++;
+        renderTheUI();
     }
 
     function renderTheUI(){
-      const toDoList = document.getElementById('toDoList');
+        const toDoList = document.getElementById('toDoList');
+            toDoList.textContent = '';
+        toDos.forEach(function(toDo){
+            const newLi = document.createElement('li');
+            const checkbox = document.createElement('input');
+            const delButton = document.createElement('button');
+            checkbox.type = "checkbox";
+            newLi.textContent = toDo.title;
+            delButton.textContent = "Delete";
+            toDoList.appendChild(newLi);
+            newLi.appendChild(checkbox);
+            newLi.appendChild(delButton);
+        });
 
-      toDoList.textContent = '';
-//the forEach() method
-      toDos.forEach(function(toDo) {
-        const newLi = document.createElement('li');
-        const checkbox = document.createElement('input');
-        checkbox.type= "checkbox";
-//adding the submitted to do to the ul as an li
-        newLi.textContent = toDo.title;
-
-        toDoList.appendChild(newLi);
-        newLi.appendChild(checkbox);
-      });
-    }
-//listens for the event-- when the user clicks submit
-    addToDoForm.addEventListener('submit', event => {
-//keeps the page from reloading when the submit button is clicked
-      event.preventDefault();
-      createNewToDo();
+    delButton.addEventListener('click', event => {
+        let newArray = toDos.filter(toDo => {return toDo.id !== id});
+        toDos.splice(toDo.id, 1);
+        toDos = newArray;
     });
+
+    }
+
+    addToDoForm.addEventListener('submit', event => {
+        event.preventDefault();
+        createNewToDo();
+    });
+
 
     renderTheUI();
 }
 
-window.onload = function(){
-  onReady();
+window.onload = function() {
+    onReady();
 };
