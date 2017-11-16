2d4ca50d846cb182501ff0b8c00f51c291d1b11b
dulcessss <dulce.aguilera25@gmail.com>
2017-11-15 21:36:05 -0600

    Edits for review. Version 2.

--- app.js
+++ app.js
@@ -1,50 +1,60 @@
 function onReady() {
-  let newNum = 0;
-    let toDos = [];
-    const addToDoForm = document.getElementById('addToDoForm');
+
+  let toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
+    let newNum = 0;
+
     function createNewToDo(){
         const newToDoText = document.getElementById('newToDoText');
         if(!newToDoText.value){return;}
+
         toDos.push ({
             title: newToDoText.value,
             complete: false,
-      id: newNum
+            id: newNum
         });
-        newToDoText.value='';
-    newNum++;
+        newNum++;
+
+        newToDoText.value = '';
         renderTheUI();
     }
 
     function renderTheUI(){
         const toDoList = document.getElementById('toDoList');
+
             toDoList.textContent = '';
+
         toDos.forEach(function(toDo){
             const newLi = document.createElement('li');
             const checkbox = document.createElement('input');
             const delButton = document.createElement('button');
+
             checkbox.type = "checkbox";
+
             newLi.textContent = toDo.title;
             delButton.textContent = "Delete";
+
             toDoList.appendChild(newLi);
             newLi.appendChild(checkbox);
             newLi.appendChild(delButton);
+
+            delButton.addEventListener('click', event => {
+                let newArray = toDos.filter(remove => toDo.id === newNum);
+                toDos.splice(toDo.id, 1);
+                
+                    renderTheUI();
+            });
         });
-
-    delButton.addEventListener('click', event => {
-        let newArray = toDos.filter(toDo => {return toDo.id !== id});
-        toDos.splice(toDo.id, 1);
-        toDos = newArray;
-    });
-
     }
 
     addToDoForm.addEventListener('submit', event => {
         event.preventDefault();
         createNewToDo();
+
     });
 
+renderTheUI();
 
-    renderTheUI();
 }
 
 window.onload = function() {
